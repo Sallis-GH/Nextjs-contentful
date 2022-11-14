0809293a40b768af13eb8f15a80f5aa76bc51e66
Sallis <Salamonerorvik@gmail.com>
2022-11-14 21:03:12 +0100

    added api/products

---
+++ jam-stack/pages/api/products.js
@@ -0,0 +1,18 @@
+import * as dotenv from 'dotenv'
+dotenv.config()
+
+const contentful = require('contentful')
+
+const client = contentful.createClient({
+  space: '5modqruhhzwo',
+  environment: 'master',
+  accessToken: process.env.PRODUCTS_PUBLISH_API_KEY
+})
+
+
+export default function handler(req, res) {
+  client.getEntries()
+    .then(response => response.items.map(item => item.fields))
+    .then(products => res.json(products))
+    .catch(console.error)
+}
--- jam-stack/pages/index.js
+++ jam-stack/pages/index.js
@@ -1,7 +1,35 @@
-
+import { useEffect, useState } from "react"
+import Card from '../components/Card'
 
 export default function Home() {
+
+  const [products, setProducts] = useState()
+
+
+  const fetchData = async (url) => {
+    let data = await fetch(url)
+    data = await data.json();
+    return data;
+  }
+
+  useEffect(() => {
+    fetch('/api/products')
+      .then(data => data.json())
+      .then(products => setProducts(products))
+  }, []);
+
+  console.log(products?.[0]);
+
   return (
-    <h1>Hello World</h1>
+    <>
+      {products?.map((product, index) => (
+        <Card key={index}
+          id={product.id}
+          name={product.name}
+          description={product.description}
+          image={product.image.fields.file.url}
+        />
+      ))}
+    </>
   )
 }
