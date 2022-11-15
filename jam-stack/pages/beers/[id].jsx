94d30a0652d79213cef310d28327f63b48e06ab2
Sallis <Salamonerorvik@gmail.com>
2022-11-15 09:31:57 +0100

    made routing to specific beer

--- jam-stack/pages/beers/[id].jsx
+++ jam-stack/pages/beers/[id].jsx
@@ -1,45 +1,29 @@
 import React, { useEffect, useState } from 'react'
 import { useRouter } from 'next/router'
-// import Card from '../../components/Card'
+import Card from '../../components/Card'
+import Navbar from '../../components/Navbar'
 
 const BeerPage = () => {
-  const router = useRouter() 
+  const router = useRouter()
   const id = router.query.id
-  const [beers, setBeers] = useState([{name: 'porcodio'}])
+  const [beers, setBeers] = useState()
 
-      const fetchk = async () => {
-        // let data = await fetch('/api/products')
-        // data = await data.json()
-        // data = data.filter(beer => beer.id == id)
-        // console.log(await data, 'popp');
-        fetch('/api/products')
-          .then(data => data.json())
-          .then(beers => beers.filter(beer => beer.id == id))
-          .then(beer => {
-            console.log(beer)
-            setBeers(beer)})
-      }
+  useEffect(() => {
+    fetch('/api/products')
+    .then(data => data.json())
+    .then(beers => beers.filter(beer => beer.id == id))
+    .then(beer =>  setBeers(beer))
+  }, [id])
 
-      useEffect(() =>{
-        fetchk();
-      }, [])
-
-      // setTimeout(() => {
-      //   console.log(beers)
-      // }, 1000);
   return (
-    // <Card
-    //   id={beers.id}
-    //   name={beers.name}
-    //   description={beers.description}
-    //   image={beers.image.fields.file.url}
-    // />
     <>
-    
-      <h1>DIO CANE</h1>
-      <h1>PORCO DIO</h1>
-      <h1>DIO BESTIA</h1>
-      <h1>{beers?.[0]?.name}</h1>
+      <Navbar />
+      <Card
+        id={beers?.[0]?.id}
+        name={beers?.[0]?.name}
+        description={beers?.[0]?.description}
+        image={beers?.[0]?.image.fields.file.url}
+      />
     </>
   )
 }
